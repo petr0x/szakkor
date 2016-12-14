9cb20de2df39c22eb7f21d5376b4f90f694af30f
pvarga <peter.varga@nokia.com>
2016-12-14 16:09:52 +0100

    Próba

---
+++ Terrarium.ino
@@ -0,0 +1,130 @@
+/*
+  Web Server
+
+ A simple web server that shows the value of the analog input pins.
+ using an Arduino Wiznet Ethernet shield.
+
+ Circuit:
+ * Ethernet shield attached to pins 10, 11, 12, 13
+ * Analog inputs attached to pins A0 through A5 (optional)
+
+ created 18 Dec 2009
+ by David A. Mellis
+ modified 9 Apr 2012
+ by Tom Igoe
+ modified 02 Sept 2015
+ by Arturo Guadalupi
+
+ */
+
+#include <SPI.h>
+#include <Ethernet.h>
+boolean incoming = 0;
+
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {
+  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02
+};
+IPAddress ip(192, 168, 1, 202);
+
+// Initialize the Ethernet server library
+// with the IP address and port you want to use
+// (port 80 is default for HTTP):
+EthernetServer server(80);
+
+void setup() {
+  pinMode(2, OUTPUT);
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+}
+
+
+void loop() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+    float temp = analogRead(0);  
+          temp = (temp/1024)*125;
+          client.print("Terrarium temp: ");
+          client.print(temp);
+          if( temp < 21 ) digitalWrite(3, HIGH);
+          if( temp > 29 ) digitalWrite(3, LOW); 
+          client.print(" C ");
+          client.println("<br />");
+          client.println("<a href=\"/$1\"\">Turn On LED</a>");
+          client.println("<br />");
+          client.println("<a href=\"/$2\"\">Turn Off LED</a>");
+          client.println("</html>");
+          break;
+        }
+        if(incoming && c == ' '){ 
+          incoming = 0;
+        }
+        if(c == '$'){ 
+          incoming = 1; 
+        }
+        
+        //Checks for the URL string $1 or $2
+        if(incoming == 1){
+          Serial.println(c);
+          
+          if(c == '1'){
+            Serial.println("ON");
+            digitalWrite(2, HIGH);
+          }
+          if(c == '2'){
+            Serial.println("OFF");
+            digitalWrite(2, LOW);
+          }
+        
+        }
+          
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+    Ethernet.maintain();
+   
+  }
+}
+
